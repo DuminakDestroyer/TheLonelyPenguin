74db7e5f4ad2d762028137bf11fc09c0d3af7ef9
DuminakDestroyer <dom.mamis@senti.com.ph>
2017-07-14 01:31:26 +0800

    layout fix

--- tlp/settings.py
+++ tlp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p(e(6rg2-j9=8pp5gzprty&@+mp833$4l3p#5v-i+e_phy7zk$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
